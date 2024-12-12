526fb05361d0af92a0247b77c620d2d2845ed644
jiangtao <cutesimba@163.com>
2024-12-12 14:50:57 +0800

    style: 优化对话页面布局，增加动画

--- tailwind.config.js
+++ tailwind.config.js
@@ -108,7 +108,48 @@ export default {
 				border: 'hsl(var(--sidebar-border))',
 				ring: 'hsl(var(--sidebar-ring))',
 			},
-  		}
+  		},
+		keyframes: {
+			'accordion-down': {
+				from: {
+					height: '0'
+				},
+				to: {
+					height: 'var(--radix-accordion-content-height)'
+				}
+			},
+			'accordion-up': {
+				from: {
+					height: 'var(--radix-accordion-content-height)'
+				},
+				to: {
+					height: '0'
+				}
+			},
+			'fade-up': {
+				'0%': { opacity: 0, transform: 'translateY(20px)' },
+				'100%': { opacity: 1, transform: 'translateY(0)' },
+			},
+			'fade-in': {
+				'0%': { opacity: 0 },
+				'100%': { opacity: 1 },
+			},
+			ripple: {
+				'0%, 100%': {
+					transform: 'translate(-50%, -50%) scale(1)'
+				},
+				'50%': {
+					transform: 'translate(-50%, -50%) scale(0.9)'
+				}
+			},
+		},
+		animation: {
+			ripple: 'ripple var(--duration,2s) ease calc(var(--i, 0)*.2s) infinite',
+			'accordion-down': 'accordion-down 0.2s ease-out',
+			'accordion-up': 'accordion-up 0.2s ease-out',
+			'fade-up': 'fade-up 0.7s ease-out',
+			'fade-in': 'fade-in 1s ease-out forwards',
+		}
   	}
   },
   plugins: [require("tailwindcss-animate"), require('@tailwindcss/typography')],
