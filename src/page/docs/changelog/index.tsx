f25fa44d9561c67a168b23902e196b3113599fcd
jiangtao <cutesimba@163.com>
2024-12-17 14:44:32 +0800

    style: 固定header，自定义全局滚动条

--- src/page/docs/changelog/index.tsx
+++ src/page/docs/changelog/index.tsx
@@ -34,7 +34,7 @@ const ChangeLog = () => {
             <Helmet>
                 <title>更新日志</title>
             </Helmet>
-            <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+            <header className="flex sticky top-0 bg-background h-16 shrink-0 items-center gap-2 border-b px-4">
                 <div className="flex flex-1 items-center gap-2 px-3">
                     <SidebarTrigger className="-ml-1"/>
                     <Separator orientation="vertical" className="mr-2 h-4"/>
--- src/page/docs/introduction/index.tsx
+++ src/page/docs/introduction/index.tsx
@@ -77,7 +77,7 @@ const Introduction = () => {
             <Helmet>
                 <title>简介</title>
             </Helmet>
-            <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+            <header className="flex sticky top-0 bg-background h-16 shrink-0 items-center gap-2 border-b px-4">
                 <div className="flex flex-1 items-center gap-2 px-3">
                     <SidebarTrigger className="-ml-1"/>
                     <Separator orientation="vertical" className="mr-2 h-4"/>
